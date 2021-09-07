f5d92eccf8f1200341e09cf120da290fd04e209f
Evan Pavlica <evan@pavlica.us>
2021-09-07 16:46:15 -0700

    Switch to setup.py for installing from github

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name='runner_client',
+    version='1.0.0',
+    author='Sony Pictures - Digital Media Group',
+    author_email='runner_help@spe.sony.com',
+    description='API client for Sony Pictures Runner / Compass API',
+    long_description='file: README.md',
+    long_description_content_type='text/markdown',
+    url='https://github.com/spedmg/runner-client-py',
+    project_urls = {
+        'Bug Tracker': 'https://github.com/spedmg/runner-client-py/issues'
+        },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'
+        ],
+    packages=setuptools.find_packages(include=['runner_client', 'runner_client.*']),
+    install_requires=['attrs', 'requests']
+    )
