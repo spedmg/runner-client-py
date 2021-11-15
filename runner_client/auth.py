4e9276e5b5abcafa6631aa6320efbb7a870745fb
Evan Pavlica <evan@pavlica.us>
2021-11-15 15:58:36 -0800

    Add authorization grant method for oauth

--- runner_client/auth.py
+++ runner_client/auth.py
@@ -2,12 +2,11 @@ from datetime import datetime
 import requests
 from . import config
 
-
 __AUTHDATA = {
-	'__current_token': None,
-	'__current_refresh_token': None,
-	'__token_expiry': 0,
-    }
+        '__current_token': None,
+        '__current_refresh_token': None,
+        '__token_expiry': 0,
+        }
 
 def access_token():
     if token_expired():
@@ -21,24 +20,47 @@ def token_expired():
         return (now + 5) > expiry
     return True
 
+def authorization_grant(code, redirect_uri):
+    '''
+    For authorization-grant oauth flows
+    {"client_id": "XXXX",
+     "client_secret": "XXX",
+     "redirect_uri": "https://your.redirect.uri",
+     "code": "code_from_step_1",
+     "grant_type": "authorization_code"}
+    '''
+    return __auth_request({
+        'client_id':     config.client_id,
+        'client_secret': config.client_secret,
+        'redirect_uri':  redirect_uri,
+        'code':          code,
+        'grant_type':    'authorization_code'
+        })
+
 def __fetch_new_token():
-    response = None
-    url = f'{config.base_url}/oauth/token'
     if __AUTHDATA['__current_refresh_token']:
         # refresh API token
-        response = requests.post(url, json={
+        __auth_request({
             'grant_type': 'refresh_token',
             'refresh_token': __AUTHDATA['__current_refresh_token'],
             })
     else:
         # New API Session
-        response = requests.post(url, json={
+        __auth_request({
             'grant_type': 'password',
             'username': config.username,
             'password': config.password,
             })
+
+def __auth_request(body):
+    url = f'{config.base_url}/oauth/token'
+    response = requests.post(url, json=body)
+
     if response.ok:
         response_data = response.json()
         __AUTHDATA['__current_token'] = response_data['access_token']
         __AUTHDATA['__current_refresh_token'] = response_data['refresh_token']
         __AUTHDATA['__token_expiry'] = response_data['created_at'] + response_data['expires_in']
+        return response_data
+
+    raise Exception(f'Runner authentication failed: {response.text}')
--- runner_client/config.py
+++ runner_client/config.py
@@ -2,5 +2,7 @@ import os
 
 base_url = os.environ.get('RUNNER_BASE_URL', 'https://sonypicturesrunner.com')
 
-username = os.environ.get('RUNNER_USERNAME')
-password = os.environ.get('RUNNER_PASSWORD')
+username      = os.environ.get('RUNNER_USERNAME')
+password      = os.environ.get('RUNNER_PASSWORD')
+client_id     = os.environ.get('RUNNER_CLIENT_ID')
+client_secret = os.environ.get('RUNNER_CLIENT_SECRET')
